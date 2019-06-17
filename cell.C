6a091ccd42486d0b03ad6e8eb462fbd37bd07230
flyntga <austin.flynt@jacobs.com>
2019-06-17 17:23:51 -0500

    checked invariants for cell constructors

--- cell.C
+++ cell.C
@@ -9,28 +9,6 @@ using std::endl;
 using std::vector;
 using std::setw;
 
-// ctor given row and column
-Cell::Cell(const int r,const int c)
-{
-  cell_num = r * 9 + c;
-  row = r;
-  col = c;
-
-  int bi = row / 3;
-  int bj = col / 3;
-
-  block = 3*bi + bj;
-
-  //value = a.get_Prc(row, col);
-  //if(value != 0) // has a value
-  //  mark.clear();
-
-  value = 0;
-
-  for(int i = 0; i < 9; i++)
-    mark.push_back(i+1);
-}
-
 // default ctor
 Cell::Cell()
 {
@@ -39,6 +17,63 @@ Cell::Cell()
   col      = -1;
   block    = -1;
   value    = -1;
+
+  for(int i = 0; i < 9; i++)
+    mark.push_back(i+1);
+}
+
+// ctor given cell num
+Cell::Cell(const int cn)
+{
+  // invariant is cell num must be >= 0 && < 81
+  if(cn < 0 || cn > 80)
+  {
+    cell_num = -1;  //invalid default
+    row      = -1;
+    col      = -1;
+    block    = -1;
+    value    = -1;
+  }
+  else{
+    cell_num = cn;
+    row = cell_num / 9;
+    col = cell_num % 9;
+
+    int bi = row / 3;
+    int bj = col / 3;
+
+    block = 3*bi + bj;
+    value = 0;
+  }
+
+  // pencil marks
+  for(int i = 0; i < 9; i++)
+    mark.push_back(i+1);
+}
+
+// ctor given row and column
+Cell::Cell(const int r,const int c)
+{
+  // invariant is row and cols range from 0 to 8
+  if(r < 0 || r > 8 || c < 0 || c > 8)
+  {
+    cell_num = -1;  //invalid default
+    row      = -1;
+    col      = -1;
+    block    = -1;
+    value    = -1;
+  }
+  else{
+    cell_num = r * 9 + c;
+    row = r;
+    col = c;
+
+    int bi = row / 3;
+    int bj = col / 3;
+
+    block = 3*bi + bj;
+    value = 0;
+  }
 
   for(int i = 0; i < 9; i++)
     mark.push_back(i+1);
@@ -67,31 +102,20 @@ Cell& Cell::operator=(const Cell& x)
   return *this;
 }
 
-// ctor given cell num
-Cell::Cell(const int cn)
-{
-  cell_num = cn;
-  row = cell_num / 9;
-  col = cell_num % 9;
-
-  int bi = row / 3;
-  int bj = col / 3;
-
-  block = 3*bi + bj;
-
-  //value = a.get_Prc(row, col);
-  //if(value != 0) // has a value
-  //  mark.clear();
-
-  value = 0;
-
-  for(int i = 0; i < 9; i++)
-    mark.push_back(i+1);
-}
 
 void Cell::set_value(const int n)
 {
-  value = n;
+  // valid values for n are 1-9 for numbers and 0 if not found
+  if(n < 1 || n > 9) // invalid value
+  {
+    value = 0;
+    mark.clear(); // reset marks
+    for(int i = 0; i < 9; i++)
+      mark.push_back(i+1);
+  }
+  else
+    value = n;
+
   if(n != 0)  // cell has found its value
     mark.clear(); // marks no longer needed
 }
